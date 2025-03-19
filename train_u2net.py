3bb215d0707ec6d0f7f855ab7f75a0dd0480dbd2
kancheng <zxdfgcv@gmail.com>
2025-03-19 10:16:37 +0800

    Added

--- train_u2net.py
+++ train_u2net.py
@@ -21,50 +21,39 @@ from data_loader import ToTensor
 from data_loader import ToTensorLab
 from data_loader import SalObjDataset
 
-from model import U2NET
-from model import U2NETP
+from u2net import U2NET
+from u2net import U2NETP
 
 # ------- 1. define loss function --------
 
 bce_loss = nn.BCELoss(size_average=True)
 
 def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
-
-    loss0 = bce_loss(d0,labels_v)
-    loss1 = bce_loss(d1,labels_v)
-    loss2 = bce_loss(d2,labels_v)
-    loss3 = bce_loss(d3,labels_v)
-    loss4 = bce_loss(d4,labels_v)
-    loss5 = bce_loss(d5,labels_v)
-    loss6 = bce_loss(d6,labels_v)
+    loss0 = bce_loss(d0, labels_v)
+    loss1 = bce_loss(d1, labels_v)
+    loss2 = bce_loss(d2, labels_v)
+    loss3 = bce_loss(d3, labels_v)
+    loss4 = bce_loss(d4, labels_v)
+    loss5 = bce_loss(d5, labels_v)
+    loss6 = bce_loss(d6, labels_v)
 
     loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
-    print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n"%(loss0.data.item(),loss1.data.item(),loss2.data.item(),loss3.data.item(),loss4.data.item(),loss5.data.item(),loss6.data.item()))
+    print("l0: %3f, l1: %3f, l2: %3f, l3: %3f, l4: %3f, l5: %3f, l6: %3f\n" % 
+          (loss0.data.item(), loss1.data.item(), loss2.data.item(), loss3.data.item(), loss4.data.item(), loss5.data.item(), loss6.data.item()))
 
     return loss0, loss
 
-
 # ------- 2. set the directory of training dataset --------
 
-model_name = 'u2net' #'u2netp'
+model_name = 'u2net'  # 'u2netp'
 
-# data_dir = os.path.join(os.getcwd(), 'train_data' + os.sep)
-# tra_image_dir = os.path.join('DUTS', 'DUTS-TR', 'DUTS-TR', 'im_aug' + os.sep)
-# tra_label_dir = os.path.join('DUTS', 'DUTS-TR', 'DUTS-TR', 'gt_aug' + os.sep)
-
-data_dir = os.path.join(os.getcwd(), 'data' + os.sep)
+data_dir = os.path.join(os.getcwd(), 'demo_data' + os.sep)
 tra_image_dir = os.path.join('imgs' + os.sep)
 tra_label_dir = os.path.join('masks' + os.sep)
 
 print(data_dir)
 print(tra_image_dir)
 print(tra_label_dir)
-
-# image_ext = '.jpg'
-# label_ext = '.png'
-
-# image_ext = '.jpg'
-# label_ext = '_mask.gif'
 
 image_ext = '.png'
 label_ext = '.png'
@@ -75,9 +64,8 @@ model_dir = os.path.join(os.getcwd(), 'saved_models', model_name + os.sep)
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
     print(f"已创建目录：{model_dir}")
+
 epoch_num = 2000
-# epoch_num = 100000
-# epoch_num = 5
 batch_size_train = 12
 batch_size_val = 1
 train_num = 0
@@ -92,7 +80,7 @@ for img_path in tra_img_name_list:
     aaa = img_name.split(".")
     bbb = aaa[0:-1]
     imidx = bbb[0]
-    for i in range(1,len(bbb)):
+    for i in range(1, len(bbb)):
         imidx = imidx + "." + bbb[i]
 
     tra_lbl_name_list.append(data_dir + tra_label_dir + imidx + label_ext)
@@ -110,17 +98,22 @@ salobj_dataset = SalObjDataset(
     transform=transforms.Compose([
         RescaleT(320),
         RandomCrop(288),
-        ToTensorLab(flag=0)]))
+        ToTensorLab(flag=0)
+    ])
+)
 salobj_dataloader = DataLoader(salobj_dataset, batch_size=batch_size_train, shuffle=True, num_workers=1)
 
 # ------- 3. define model --------
 # define the net
-if(model_name=='u2net'):
+if model_name == 'u2net':
     net = U2NET(3, 1)
-elif(model_name=='u2netp'):
-    net = U2NETP(3,1)
+elif model_name == 'u2netp':
+    net = U2NETP(3, 1)
 
-net.cpu()
+# 判斷是否有 GPU、若有則用 GPU 訓練，否則用 CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("使用的裝置:", device)
+net.to(device)
 
 # ------- 4. define optimizer --------
 print("---define optimizer...")
@@ -132,7 +125,6 @@ ite_num = 0
 running_loss = 0.0
 running_tar_loss = 0.0
 ite_num4val = 0
-# save_frq = 2000 # save the model every 2000 iterations
 save_frq = 200
 
 def main():
@@ -148,22 +140,19 @@ def main():
         net.train()
 
         for i, data in enumerate(salobj_dataloader):
-            ite_num = ite_num + 1
-            ite_num4val = ite_num4val + 1
+            ite_num += 1
+            ite_num4val += 1
 
             inputs, labels = data['image'], data['label']
 
-            inputs = inputs.type(torch.FloatTensor)
-            labels = labels.contiguous().type(torch.FloatTensor)
+            # 將輸入數據轉換為 FloatTensor 並移動到指定裝置（GPU 或 CPU）
+            inputs = inputs.type(torch.FloatTensor).to(device)
+            labels = labels.contiguous().type(torch.FloatTensor).to(device)
 
             # wrap them in Variable
-            if torch.cuda.is_available():
-                inputs_v, labels_v = Variable(inputs.cpu(), requires_grad=False), Variable(labels.cpu(),
-                                                                                            requires_grad=False)
-            else:
-                inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(labels, requires_grad=False)
+            inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(labels, requires_grad=False)
 
-            # y zero the parameter gradients
+            # 清空梯度
             optimizer.zero_grad()
 
             # forward + backward + optimize
@@ -173,18 +162,17 @@ def main():
             loss.backward()
             optimizer.step()
 
-            # # print statistics
+            # 累計損失
             running_loss += loss.data.item()
             running_tar_loss += loss2.data.item()
 
-            # del temporary outputs and loss
+            # 刪除暫時性變量
             del d0, d1, d2, d3, d4, d5, d6, loss2, loss
 
-            print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " % (
-            epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
+            print("[epoch: %3d/%3d, batch: %5d/%5d, ite: %d] train loss: %3f, tar: %3f " %
+                  (epoch + 1, epoch_num, (i + 1) * batch_size_train, train_num, ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
 
             if ite_num % save_frq == 0:
-
                 model_path = os.path.join(model_dir, f"{model_name}_bce_itr_{ite_num}_train_{running_loss / ite_num4val:.3f}_tar_{running_tar_loss / ite_num4val:.3f}.pth")
                 torch.save(net.state_dict(), model_path)
                 print(f"模型已保存到 {model_path}")
--- unet/__init__.py
+++ unet/__init__.py
@@ -1,2 +1,1 @@
 from .unet_model import UNET
-from .u2net_model import U2NET
